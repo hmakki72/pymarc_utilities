6d9f6b25668bcb02a1b5ef5033acdd8ec3eb4f48
Melanie Polutta <bibliotecaria.mel@gmail.com>
2024-11-01 17:34:35 -0400

    finished test and adding options for outputs--still buggy

--- test_uncombine.py
+++ test_uncombine.py
@@ -4,7 +4,7 @@
 
 
 '''
- Combine characters examples : ḥ ḍ ā ī á
+Combine characters examples : ḥ ḍ ā ī á
 Uncombine characters examples: Ḥ ḍ ā ī
 '''
 
@@ -18,7 +18,13 @@ from pymarc import *
 modf = pymarc_utilities.ENCODING()
 findit = pymarc_utilities.FIND_AND_REPLACE
 
+#If set to True, processes all files in input MARC file. 
+#If set to False, processes only records with containing specific diacritics in 100$a.
 uncombine_all = False
+#If uncombine_all is False, and ask_each_time is True, record will display and user will decide to write out.
+ask_each_time = False
+#If uncombine_all is False, and write_out_records is False, no records will be written out.
+write_out_records = False
 input_marcfile='authfiles/FASTPersonal_9.mrc'
 output_marcfile='authfiles/FASTPersonal_9.uncomb.mrc'
 
@@ -33,7 +39,7 @@ with open(input_marcfile, 'rb') as fh:
     for record in reader:
       recnum += 1 #Records count
       if uncombine_all:
-      #Uncombaine all records in the file
+      #Uncombine all records in the file
         
         try:
           '''
@@ -61,17 +67,31 @@ with open(input_marcfile, 'rb') as fh:
            Uncombine the record only if 100$a has any
            of the following combine characters āūīḥḍ
         '''
+        found = 0
+        converted = 0
         sample_field = Field(tag="100", indicators=["?", "?"],
                     subfields=[Subfield(code="a", value="[āūīḥḍ]")])
         is_found = findit.find(record, sample_field)
         if is_found:
+            found += 1
+            origauth = record.get_fields('100')[0]['a']
             extracted_rec = modf.uncombine_diacritics(record, '?')
-            print(extracted_rec)   
-            user_input = input("Save to MARC file? 'y' : ")
-            if user_input == 'y':
-               extractednum += 1
-               with open('exrt377aARA_Diac.mrc', 'ab') as out:
-                    out.write(extracted_rec.as_marc())
+            modauth = extracted_rec.get_fields('100')[0]['a']
+            #compare length of original string with uncombined diacritics string to test if uncombine happened
+            if len(origauth) < len(modauth):
+              converted += 1
+            if write_out_records:
+              if ask_each_time:
+                print (extracted_rec)
+                user_input = input("Save to MARC file? 'y' : ")
+              else:
+                 user_input = "y"
+              if user_input == 'y':
+                extractednum += 1
+                with open('exrt377aARA_Diac.mrc', 'ab') as out:
+                  out.write(extracted_rec.as_marc())
 
+    if not uncombine_all:
+       print(f"Number of diacritic records found: {found}. Number of records uncombined: {converted}.y")
     print (f"Total processed records : {extractednum} of {recnum}")
     
